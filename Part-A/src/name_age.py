82de6dc33411c67d23f0cb364d696131886fa177
suttonbehie <sutton.behie@snhu.edu>
2026-09-11 20:12:56 -0400

    Refactor current year retrieval and clean code
    Updated the method of obtaining the current year and cleaned up the code structure.

--- Part-A/src/name_age.py
+++ Part-A/src/name_age.py
@@ -16,34 +16,17 @@
  #   How old are you? 25
   #  Hello Sutton! You were born in 2001. 
 
-# === Imports ===
-from datetime import date
+from datetime import datetime‹
 
+CURRENT_YEAR = date.now().year  
 
-# === Constants ===
-CURRENT_YEAR = date.today().year  # Get current year from system as integer
-
-
-# === Main Function ===
-# def main() -> None:
- #   """Run the name-age program."""
-
-    # Get user input.
     name = input("What is your name? ")
     age = int(input(How old are you? "))
 
-    # Calculate user's approximate birth year.
     birth_year = CURRENT_YEAR - age
 
-    # Output personalized message with user's name and birth year.
     print(f"Hello {name}! You were born in {birth_year}.")
 
 
-# === Main Guard ===
-# if __name__ == "__main__":
-#    main()
 
 
-# === References ===
-# TODO: Replace with an APA-style reference for a source you used, or delete.
-# TODO: Replace with another APA-style reference, or delete this TODO line.
